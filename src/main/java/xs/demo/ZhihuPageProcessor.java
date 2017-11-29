de45f5508a67fabcb977fff6da4206b0d69e9e08
xiaoshun <495314431@qq.com>
2017-11-29 23:53:00 +0800

    zhihuPageProcessor

---
+++ src/main/java/xs/demo/ZhihuPageProcessor.java
@@ -0,0 +1,32 @@
+package xs.demo;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class ZhihuPageProcessor implements PageProcessor {
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+    public ZhihuPageProcessor() {
+    }
+
+    public void process(Page page) {
+        page.addTargetRequests(page.getHtml().links().regex("https://www\\.zhihu\\.com/question/\\d+/answer/\\d+.*").all());
+        page.putField("title", page.getHtml().xpath("//h1[@class='QuestionHeader-title']/text()").toString());
+        page.putField("question", page.getHtml().xpath("//div[@class='QuestionRichText']//tidyText()").toString());
+        page.putField("answer", page.getHtml().xpath("//div[@class='QuestionAnswer-content']/tidyText()").toString());
+        if (page.getResultItems().get("title") == null) {
+            page.setSkip(true);
+        }
+
+    }
+
+    public Site getSite() {
+        return this.site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new us.codecraft.webmagic.processor.example.ZhihuPageProcessor()).addUrl(new String[]{"https://www.zhihu.com/explore"}).run();
+    }
+}
