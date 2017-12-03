0f050efdd7a49900e5e10f19610a4cdfd0563182
xiaoshun <495314431@qq.com>
2017-12-03 23:10:17 +0800

    抓取前端渲染的页面

---
+++ src/main/java/xs/demo/AngularJsProcessor.java
@@ -0,0 +1,42 @@
+package xs.demo;
+
+import org.apache.commons.collections.CollectionUtils;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.JsonPathSelector;
+
+import java.util.List;
+
+public class AngularJsProcessor implements PageProcessor {
+    private Site site = Site.me();
+
+    private static final String ARITICALE_URL = "http://angularjs\\.cn/api/article/\\w+";
+
+    private static final String LIST_URL = "http://angularjs\\.cn/api/article/latest.*";
+
+    @Override
+    public void process(Page page) {
+        if (page.getUrl().regex(LIST_URL).match()) {
+            List<String> ids = new JsonPathSelector("$.data[*]._id").selectList(page.getRawText());
+            if (CollectionUtils.isNotEmpty(ids)) {
+                for (String id : ids) {
+                    page.addTargetRequest("http://angularjs.cn/api/article/" + id);
+                }
+            }
+        } else {
+            page.putField("title", new JsonPathSelector("$.data.title").select(page.getRawText()));
+            page.putField("content", new JsonPathSelector("$.data.content").select(page.getRawText()));
+        }
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new AngularJsProcessor()).addUrl("http://angularjs.cn/api/article/latest?p=1&s=20").run();
+    }
+}
